a5322c1a67963be61f848341f433205f9d103e01
jp-freitas <tec.joao.freitas@gmail.com>
2023-05-27 11:59:34 -0300

    Improving the application with env variables, migrations and data validation with zod

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async (request, response) => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions')
+    .where('session_id', 'bcc481a8-9af0-4066-ab15-b25073bfcac9')
+    .select('*')
+  return transactions
 })
 
 app
   .listen({
-    port: 4445,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server is running!')
