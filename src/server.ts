8944ab4424a1cc1e4eb8548c5aaf40c8c68e9400
jp-freitas <tec.joao.freitas@gmail.com>
2023-05-29 19:54:07 -0300

    Registering the transactions route and the cookie plugin from fastify

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import fastify from 'fastify'
-// import crypto from 'node:crypto'
-import { knex } from './database'
+import cookie from '@fastify/cookie'
 import { env } from './env'
 
-const app = fastify()
+import { transactionsRoutes } from './routes/transactions'
 
-app.get('/hello', async (request, response) => {
-  const transactions = await knex('transactions')
-    .where('session_id', 'bcc481a8-9af0-4066-ab15-b25073bfcac9')
-    .select('*')
-  return transactions
+const app = fastify()
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
